5583cdd95851dfc94a4e6faf1397333bbc671cdf
= <=>
2024-09-03 21:57:59 +0700

    code optimization

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { DataGrid } from "@mui/x-data-grid";
 import { columns } from "./TableColumn";
-import { DataTableType } from "../assets/dataTableType";
 import { useQuery } from "@tanstack/react-query";
 import { getAllCoin, getRate } from "../api/api";
 
 export default function TableData() {
-  const coins = useQuery({
+  const { data: coins } = useQuery({
     queryKey: ["coins"],
     queryFn: () => getAllCoin(),
     refetchInterval: 20000,
@@ -14,26 +13,20 @@ export default function TableData() {
     refetchOnReconnect: true,
   });
 
-  const rates = useQuery({
+  const { data: rates } = useQuery({
     queryKey: ["rates"],
     queryFn: () => getRate(),
   });
 
-  const renderCell = (params: any) => {
-    if (rates?.data?.krw_vnd_rate > params.value) {
-      return (
-        <span className="text-[10px] md:text-base text-red-500">
-          {params.value}
-        </span>
-      );
-    } else {
-      return (
-        <span className="text-[10px] md:text-base text-blue-600">
-          {params.value}
-        </span>
-      );
-    }
-  };
+  const renderCell = (params: any) => (
+    <span
+      className={`text-[10px] md:text-base ${
+        rates?.krw_vnd_rate > params.value ? "text-red-500" : "text-blue-600"
+      }`}
+    >
+      {params.value}
+    </span>
+  );
 
   const updatedColumns = columns.map((col) => {
     if (col.field === "upbit_rate" || col.field === "bithumb_rate") {
@@ -45,38 +38,33 @@ export default function TableData() {
     return { ...col };
   });
 
-  const coinData = coins?.data?.message;
-  const listToken = coinData ? Object.keys(coinData) : [];
-
-  const rows = listToken?.map((item) => {
-    const rows: DataTableType = {
-      token: coinData[item]?.coin_symbol,
-      bnb: coinData[item]?.price_at_binance,
-      upbit: coinData[item]?.price_at_upbit,
-      bithumb: coinData[item]?.price_at_bithump,
-      upbit_rate: Number(coinData[item]?.upbit_rate.toFixed(3)),
-      bithumb_rate: Number(coinData[item]?.bithumb_rate.toFixed(3)),
-    };
-    return rows;
-  });
+  const rows = Object.entries(coins?.message || {}).map(
+    ([, data]: [string, any]) => ({
+      token: data?.coin_symbol || "no data",
+      bnb: data?.price_at_binance || "no data",
+      upbit: data?.price_at_upbit || "no data",
+      bithumb: data?.price_at_bithump || "no data",
+      upbit_rate: Number(data?.upbit_rate.toFixed(3)) || "no data",
+      bithumb_rate: Number(data?.bithumb_rate.toFixed(3)) || "no data",
+    })
+  );
 
   return (
     <>
       <DataGrid
         columns={updatedColumns}
         rows={rows}
-        hideFooter={true}
-        disableColumnResize={true}
+        hideFooter
+        disableColumnResize
         getRowId={(row) => row.token}
         sx={{
           "& .MuiDataGrid-cell": {
             padding: "0px",
           },
           "& .MuiDataGrid-columnHeader": {
-            padding: "0px", 
+            padding: "0px",
           },
           border: "none",
-          
         }}
       ></DataGrid>
     </>
